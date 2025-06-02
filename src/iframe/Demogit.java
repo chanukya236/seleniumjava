a7406a871e13660e46aba030cde5ba301ff62a4a
chanukya236 <chanukya236@gmail.com>
2025-06-02 11:12:51 +0530

    Newcon900

--- src/iframe/Demogit.java
+++ src/iframe/Demogit.java
@@ -1,14 +1,21 @@
 package iframe;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class Demogit {
+	WebDriver driver;
+
   @Test
   public void f() {
+	  driver.get("https://www.google.com");
   }
   @BeforeTest
   public void beforeTest() {
+	  driver= new ChromeDriver();
+	  driver.manage().window().maximize();
   }
 
 }
