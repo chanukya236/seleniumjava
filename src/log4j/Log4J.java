8ef180894f0723ce7b4147ba45e94bf530b2ed59
chanukya236 <chanukya236@gmail.com>
2025-06-02 11:20:11 +0530

    Log4j code updated by girija

--- src/log4j/Log4J.java
+++ src/log4j/Log4J.java
@@ -4,8 +4,9 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 
 public class Log4J {
+  WebDriver driver;
   @Test
-  public void f() {
+  public void google() {
   }
   @BeforeTest
   public void beforeTest() {
