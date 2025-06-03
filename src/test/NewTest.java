b3f7788ce0062fc10230f55251498696bf35b2ca
chanukya236 <chanukya236@gmail.com>
2025-06-03 10:57:55 +0530

    Update NewTest.java Raju

--- src/test/NewTest.java
+++ src/test/NewTest.java
@@ -12,8 +12,8 @@ public class NewTest {
   @Test
   public void xpath() {
 	  driver.get("http://techlearn.in/admin");
-	  driver.findElement(By.xpath("/html/body/div[1]/form/p[1]/input")).sendKeys("harish");
-	  driver.findElement(By.xpath("//*[@id=\"user_pass\"]")).sendKeys("harish@123");
+	  driver.findElement(By.xpath("/html/body/div[1]/form/p[1]/input")).sendKeys("raju");
+	  driver.findElement(By.xpath("//*[@id=\"user_pass\"]")).sendKeys("raju@123");
 	  driver.findElement(By.xpath("//*[@id=\"rememberme\"]")).click();
   }
   @BeforeTest
