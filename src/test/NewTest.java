8f2b026d0770944fbfab95f81534c421b2bb3de4
chanukya236 <chanukya236@gmail.com>
2025-06-04 09:32:14 +0530

    Change

--- src/test/NewTest.java
+++ src/test/NewTest.java
@@ -12,9 +12,7 @@ public class NewTest {
   @Test
   public void xpath() {
 	  driver.get("http://techlearn.in/admin");
-	  driver.findElement(By.xpath("/html/body/div[1]/form/p[1]/input")).sendKeys("raju");
-	  driver.findElement(By.xpath("//*[@id=\"user_pass\"]")).sendKeys("raju@123");
-	  driver.findElement(By.xpath("//*[@id=\"rememberme\"]")).click();
+	  
   }
   @BeforeTest
   public void beforeTest() {
